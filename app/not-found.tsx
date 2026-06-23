e1745853744f9f0dfac1f24301d54c565d7face3
LarsvandenBosch <lars@skeps.nl>
2026-06-23 09:34:41 +0200

    test: 500 error vercel homepage

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,17 @@
 import { Hero } from "@/components/section/hero";
+import { Navbar } from "@/components/section/navbar";
 
 export default function Custom404() {
     return (
-        <Hero
-            hasTagline={false}
-            heading="Pagina niet gevonden"
-            description="De pagina die je zoekt bestaat niet of is veranderd."
-            buttonLabel="Ga naar home"
-            buttonHref="/"
-        />
+        <>
+            <Navbar />
+            <Hero
+                hasTagline={false}
+                heading="Pagina niet gevonden"
+                description="De pagina die je zoekt bestaat niet of is veranderd."
+                buttonLabel="Ga naar home"
+                buttonHref="/"
+            />
+        </>
     )
 }
